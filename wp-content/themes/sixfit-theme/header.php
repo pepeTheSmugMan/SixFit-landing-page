a47431f6030942add2ec66087a504811bd084c9d
Maksymilian Lipke <maxlipmux@gmail.com>
2025-07-08 09:23:35 +0200

    changed colors of the menu, added placeholder logo

--- wp-content/themes/sixfit-theme/header.php
+++ wp-content/themes/sixfit-theme/header.php
@@ -29,6 +29,10 @@
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
+			<img 
+				src="<?php echo get_template_directory_uri(); ?>/assets/placeholder.png"
+				alt="icon"
+				loading="lazy">
 			<?php
 			the_custom_logo();
 			if ( is_front_page() && is_home() ) :
