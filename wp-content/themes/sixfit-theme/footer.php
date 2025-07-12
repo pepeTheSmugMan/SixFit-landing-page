729571d5271ae5d3b887fe1537a5a3bd6dbfb399
Maksymilian Lipke <maxlipmux@gmail.com>
2025-07-12 12:04:46 +0200

    fixed site overflow on mobile

--- wp-content/themes/sixfit-theme/footer.php
+++ wp-content/themes/sixfit-theme/footer.php
@@ -17,19 +17,6 @@
 				<?php dynamic_sidebar( 'footer-info-wrapper' ); ?>
 			<?php endif; ?>
 		</div>
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'sixfit-theme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'sixfit-theme' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'sixfit-theme' ), 'sixfit-theme', '<a href="https://github.com/pepeTheSmugMan">Maksymilian Lipke</a>' );
-				?>
-		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
